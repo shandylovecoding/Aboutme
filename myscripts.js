ee484a0b2042ec19dcfabbd6ab900bed42885f10
shandylovecoding <84694770+shandylovecoding@users.noreply.github.com>
2021-05-25 20:13:42 +0800

    favicon and hamburger animation done

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,26 @@
-$('.menu a').on('click', function(e){
-    if (this.hash !==''){
-        e.preventDefault();
+-$('.menu a').on('click', function(e){
+  if (this.hash !==''){
+      e.preventDefault();
+var button = document.getElementById( 'menu-toggle' );
 
-        const hash = this.hash;
+      const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        },
-        800);
-    }
+      $('html, body').animate({
+          scrollTop: $(hash).offset().top
+      },
+      800);
+  }
 })
+
+////////////////hamburger///////////////
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
