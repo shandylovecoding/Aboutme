e5905376d5df44eb3aadcb7e655da669ae28e502
shandy <shandylovecoding@gmail.com>
2021-12-22 04:21:05 +0800

    working on working experience

--- myscripts.js
+++ myscripts.js
@@ -89,6 +89,60 @@ document.addEventListener("DOMContentLoaded",function(){
   })
   ;
 });
+/////////////////////
 
+console.clear();
+
+const grid = document.querySelector('.tab_inner');
+const tabs = document.querySelectorAll('[role="tab"]');
+const tabList = document.querySelector('[role="tablist"]');
+
+let tabFocus = 0;
+
+for (const [index, tab] of tabs.entries()) {
+  tab.style.setProperty('--i', index + 1);
+}
+
+tabList.addEventListener('click', ({ target }) => {
+  //const tg = e.target;
+  //const parent = tg.parentNode;
+
+  if(target.tagName === 'BUTTON') {
+    const index = [...tabList.children].indexOf(target);
+    tabList.style.setProperty('--curr', index);
+    tabList.querySelector('[aria-selected="true"]').setAttribute('aria-selected', false);
+    //parent.querySelectorAll('[aria-selected="true"]').forEach(t => t.setAttribute("aria-selected", false));
+
+    target.setAttribute('aria-selected', true);
+    grid.dataset.state = target.dataset.setstate;
+  }
+}, false);
+
+
+tabList.addEventListener("keydown", e => {
+  // Move right
+  if (e.keyCode === 40 || e.keyCode === 38) {
+    e.preventDefault();
+    tabs[tabFocus].setAttribute("tabindex", -1);
+    if (e.keyCode === 40) {
+      tabFocus++;
+      // If we're at the end, go to the start
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+      // Move left
+    } else if (e.keyCode === 38) {
+      tabFocus--;
+      // If we're at the start, move to the end
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].click();
+    tabs[tabFocus].focus();
+  }
+});
 
 
